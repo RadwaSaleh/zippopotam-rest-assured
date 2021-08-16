2db4e6b178aa5e2dd7c484a4047203f9ccc7de73
rsaleh <salehradwa@gmail.com>
2021-08-16 16:19:38 +0200

    define RequestSpecification to reuse base uri

--- src/test/java/GermanyZipCodeTest.java
+++ src/test/java/GermanyZipCodeTest.java
@@ -1,9 +1,20 @@
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class GermanyZipCodeTest {
+    private static RequestSpecification requestSpec;
+
+    @BeforeClass
+    public static void createRequestSpecification(){
+        requestSpec = new RequestSpecBuilder()
+                .setBaseUri("http://zippopotam.us")
+                .build();
+    }
 
     @DataProvider
     public static Object[][] zipCodesAndPlaces(){
@@ -15,10 +26,12 @@ public class GermanyZipCodeTest {
     }
 
     @Test(dataProvider = "zipCodesAndPlaces")
-    public void requestBerlinZipCode10627(String countryCode, String zipCode, String expectedCity, String expectedState){
-        given().pathParam("countryCode", countryCode).pathParam("zipCode", zipCode)
-                .when().get("http://zippopotam.us/{countryCode}/{zipCode}")
+    public void verifyDifferentZipCodesPlaces(String countryCode, String zipCode, String expectedCity, String expectedState){
+        given().spec(requestSpec)
+                .pathParam("countryCode", countryCode).pathParam("zipCode", zipCode)
+                .when().get("/{countryCode}/{zipCode}")
                 .then().assertThat()
+                .statusCode(200)
                 .body("places[0].'place name'", equalTo(expectedCity))
                 .body("places[0].state", equalTo(expectedState));
     }
