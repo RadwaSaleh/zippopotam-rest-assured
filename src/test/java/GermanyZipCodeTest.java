0d9a2b47cef2014405ce233bca62794d07aaec70
rsaleh <salehradwa@gmail.com>
2021-08-16 12:07:02 +0200

    parameterize tests

--- src/test/java/GermanyZipCodeTest.java
+++ src/test/java/GermanyZipCodeTest.java
@@ -1,14 +1,25 @@
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class GermanyZipCodeTest {
 
-    @Test
-    public void requestBerlinZipCode10627(){
-        given().when().get("http://zippopotam.us/de/10627")
+    @DataProvider
+    public static Object[][] zipCodesAndPlaces(){
+        return new Object[][] {
+                {"de", "10627", "Berlin", "Berlin"},
+                {"de", "40210", "Düsseldorf", "Nordrhein-Westfalen"},
+                {"de", "80333", "München", "Bayern"},
+        };
+    }
+
+    @Test(dataProvider = "zipCodesAndPlaces")
+    public void requestBerlinZipCode10627(String countryCode, String zipCode, String expectedCity, String expectedState){
+        given().pathParam("countryCode", countryCode).pathParam("zipCode", zipCode)
+                .when().get("http://zippopotam.us/{countryCode}/{zipCode}")
                 .then().assertThat()
-                .body("places[0].'place name'", equalTo("Berlin"))
-                .body("places[0].state", equalTo("Berlin"));
+                .body("places[0].'place name'", equalTo(expectedCity))
+                .body("places[0].state", equalTo(expectedState));
     }
 }
